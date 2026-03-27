a0ef70743bf22ea64c4e4a96f6631453b6375a4f
jackjohn7 <branchjohnchristopher@gmail.com>
2026-03-26 23:05:23 -0500

    fix: re-enable GDM and Gnome for now

--- modules/features/layers/base.nix
+++ modules/features/layers/base.nix
@@ -19,6 +19,9 @@
       LC_TIME = "en_US.UTF-8";
     };
 
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
+
     # Enable the X11 windowing system.
     services.xserver.enable = true;
 
