7cada7d4b5250f8f12391b61b77c4959f86a3b94
jackjohn7 <70782491+jackjohn7@users.noreply.github.com>
2026-03-28 20:40:26 -0500

    fix cursors and wallpapers

--- modules/features/layers/cursors.nix
+++ modules/features/layers/cursors.nix
@@ -8,22 +8,25 @@
       ...
     }:
     {
-      hjem.users."${config.username}" =  {
-        enable = true;
-        files = {
-          # GTK cursor settings
-          ".config/gtk-3.0/settings.ini".text = ''
-            [Settings]
-            gtk-cursor-theme-name=Bibata-Modern-Ice
-            gtk-cursor-theme-size=24
-          '';
-          # X11/Wayland cursor environment
-          ".config/environment.d/cursor.conf".text = ''
-            XCURSOR_THEME=Bibata-Modern-Ice
-            XCURSOR_SIZE=24
-            HYPRCURSOR_THEME=Bibata-Modern-Ice
-            HYPRCURSOR_SIZE=24
-          '';
+      config = {
+        environment.systemPackages = with pkgs; [ bibata-cursors ];
+        hjem.users."${config.username}" =  {
+          enable = true;
+          files = {
+            # GTK cursor settings
+            ".config/gtk-3.0/settings.ini".text = ''
+              [Settings]
+              gtk-cursor-theme-name=Bibata-Modern-Ice
+              gtk-cursor-theme-size=24
+            '';
+            # X11/Wayland cursor environment
+            ".config/environment.d/cursor.conf".text = ''
+              XCURSOR_THEME=Bibata-Modern-Ice
+              XCURSOR_SIZE=24
+              HYPRCURSOR_THEME=Bibata-Modern-Ice
+              HYPRCURSOR_SIZE=24
+            '';
+          };
         };
       };
     };
