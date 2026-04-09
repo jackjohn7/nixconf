789083d32fa48cb2a8277e39b6337073719d5bdc
jackjohn7 <70782491+jackjohn7@users.noreply.github.com>
2026-04-09 02:57:23 -0500

    add boilerplate generation CLI

---
+++ modules/shell.nix
@@ -0,0 +1,25 @@
+{ ... }:
+{
+  perSystem =
+    { pkgs, self', ... }:
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          bacon
+          cargo
+          rust-analyzer
+          rustc
+          rustfmt
+          clippy
+          glibc
+          sea-orm-cli
+          nixfmt
+          nil
+          alejandra
+          self'.packages.boilerplate
+        ];
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
+    };
+}
